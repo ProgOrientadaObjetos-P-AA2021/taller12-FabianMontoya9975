820504531f941ed03d8a80337e044091b31fc944
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2021-07-08 17:05:49 -0500

    Resolucion taller 12

--- TallerJUnit/src/paquete1/EstudianteDistancia.java
+++ TallerJUnit/src/paquete1/EstudianteDistancia.java
@@ -1,68 +1,71 @@
-
 package paquete1;
 
 import java.util.ArrayList;
 
-public class EstudianteDistancia extends Estudiante{
-       
+public class EstudianteDistancia extends Estudiante {
+
     private ArrayList<Nota> notas;
     private double mejorNota;
     private double peorNota;
     private double promedio;
-    
-    public void establecerNotas(ArrayList<Nota> ns){
+
+    public void establecerNotas(ArrayList<Nota> ns) {
         notas = ns;
     }
-    
-    public void establecerMejorNota(){
+
+    public void establecerMejorNota() {
         double n = 0;
-        for(int i = 0; i < obtenerNotas().size(); i++){
-            if(obtenerNotas().get(i).obtenerValor() > n){
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            if (obtenerNotas().get(i).obtenerValor() > n) {
                 n = obtenerNotas().get(i).obtenerValor();
             }
         }
         mejorNota = n;
     }
-    
-    public void establecerPeorNota(){
-        double n = 10;
-        for(int i = 0; i < obtenerNotas().size(); i++){
-            if(obtenerNotas().get(i).obtenerValor() < n){
+
+    public void establecerPeorNota() {
+        double n = 0;
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            if (i == 0) {
                 n = obtenerNotas().get(i).obtenerValor();
+            } else {
+                if (obtenerNotas().get(i).obtenerValor() < n) {
+                    n = obtenerNotas().get(i).obtenerValor();
+                }
             }
         }
         peorNota = n;
     }
-    
-    public void establecerPromedio(){
+
+    public void establecerPromedio() {
         double suma = 0;
-        for(int i = 0; i < obtenerNotas().size(); i++){
+        for (int i = 0; i < obtenerNotas().size(); i++) {
             suma = suma + obtenerNotas().get(i).obtenerValor();
         }
         promedio = suma / obtenerNotas().size();
     }
-    
-    public ArrayList<Nota> obtenerNotas(){
+
+    public ArrayList<Nota> obtenerNotas() {
         return notas;
     }
-    
-    public double obtenerMejorNota(){
+
+    public double obtenerMejorNota() {
         return mejorNota;
     }
-    
-    public double obtenerPeorNota(){
+
+    public double obtenerPeorNota() {
         return peorNota;
     }
-    
-    public double obtenerPromedio(){
+
+    public double obtenerPromedio() {
         return promedio;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         String cadena = String.format("%sNotas: \n", super.toString());
-        for(int i = 0; i < obtenerNotas().size(); i++){
-            cadena = String.format("%s%.1f",cadena, obtenerNotas().get(i).
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            cadena = String.format("%s%.1f", cadena, obtenerNotas().get(i).
                     obtenerValor());
         }
         cadena = String.format("%sProemdio: %.1fMejor Nota: %.1f"
@@ -70,8 +73,5 @@ public class EstudianteDistancia extends Estudiante{
                 obtenerPromedio(), obtenerMejorNota(), obtenerPeorNota());
         return cadena;
     }
-    
-    
-    
 
 }
